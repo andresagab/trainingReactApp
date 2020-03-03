caa64197391197eab4d863150e80dfee9e98403d
Andres Geovanny Angulo Botina <andrescabj981@gmail.com>
2020-03-03 12:33:08 -0500

    Componentes, props and state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import exerciseImg from './../images/exercise.png'
+import circlesImg from './../images/circles.png'
+import './../styles/Card.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Card extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-512.png'
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                image: exerciseImg
+            })
+        }, 5000);
+    }
+
     render() {
-        return <h1>card</h1>
+
+        const {title, description, img, leftColor, rightColor} = this.props;
+
+        return (
+            <div className="card mx-auto Fitness-Card" style={{backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`}}>
+                <div className='card-body'>
+                    <div className='row center'>
+                        <div className='col-6'>
+                            <img className='float-right' src={this.state.image}/>
+                        </div>
+                        <div className='col-6 Fitness-Card-Info'>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
         //return super.render();
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,32 +9,8 @@ container.appendChild(element);
 */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './components/Card';
 
-const word = 'World';
-const element = <h1>Hello {word}</h1>;
 const contaniner = document.getElementById('root');
 
-function formatName(user) {
-    return `${user.firstName} ${user.lastName}`;
-}
-
-function getGreeting(user) {
-    if (user) return <h1>Hello, {formatName(user)}</h1>;
-    return <h1>Hello Stranger.</h1>
-}
-
-const user = {
-    firstName: 'Harper',
-    lastName: 'Perez',
-    avatar: 'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-512.png'
-};
-
-const namePerson = (
-    <div>
-        <h1>{getGreeting(user)}</h1>
-        <img src={user.avatar}/>
-    </div>
-);
-
-ReactDOM.render(element, contaniner);
-ReactDOM.render(namePerson, contaniner);
+ReactDOM.render(<Card title="Mi titulo" description="mi descripción" img="" leftColor="#A74CF2" rightColor="#617BFB"/>, contaniner);
