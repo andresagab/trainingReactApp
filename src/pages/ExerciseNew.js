31b4286569ad03268e00e1aa7b2a9463a26cf39e
Andres Geovanny Angulo Botina <andrescabj981@gmail.com>
2020-04-14 19:45:07 -0500

    Control de componentes, eventos de formulario

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -3,16 +3,46 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 class ExerciseNew extends React.Component{
 
+    state = {};
+
     render() {
         return (
-            <div>
-                <button className='btn btn-success' onClick={this.handleClick}>Send</button>
+            <div className='container'>
+                <form onSubmit={this.handleSubmit}>
+                    <div className='form-group'>
+                        <input className='form-control' type='text' name='tittle' placeholder='Tittle' value={this.state.tittle} onChange={this.handleChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control' type='text' name='description' placeholder='Description' value={this.state.description} onChange={this.handleChange}/>
+                    </div>
+                    <div className='form-group'>
+                        <input className='form-control' type='text' name='img' placeholder='img' value={this.state.img} onChange={this.handleChange}/>
+                    </div>
+                    <div className='form-row'>
+                        <div className='col'>
+                            <input className='form-control' type='text' name='leftColor' placeholder='Left Color' value={this.state.leftColor} onChange={this.handleChange}/>
+                        </div>
+                        <div className='col'>
+                            <input className='form-control' type='text' name='rightColor' placeholder='Right Color' value={this.state.rightColor} onChange={this.handleChange}/>
+                        </div>
+                    </div>
+                    <button className='btn btn-primary float-right' type='submit'>Submit</button>
+                </form>
             </div>
         );
     }
 
-    handleClick = () => {
-        console.log(this);
+    handleChange = e => {
+        // console.log(`${e.target.name}: ${e.target.value}`);
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    handleSubmit = e => {
+        //Cancelamos el reload de la página
+        e.preventDefault();
+        console.log(this.state);
     }
 
 }
